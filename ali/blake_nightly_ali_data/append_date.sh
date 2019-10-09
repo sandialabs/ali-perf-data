3000fd433ed8b2c86089306af50f21b7918ebae2
Irina K. Tezaur <ikalash@sandia.gov>
2019-10-09 15:32:51 -0700

    Corrections.

--- ali/blake_nightly_ali_data/append_date.sh
+++ ali/blake_nightly_ali_data/append_date.sh
@@ -7,12 +7,12 @@ if test -f "$FILE"; then
   echo "$FILE file exists"
   now=$(date +"%m_%d_%Y")
   echo $now
-  mv Blob_PerfTestsBlake.html Blob_PerfTestsBlake_$now.html
-  git add Blob_PerfTestsBlake_$now.html
+  mv Ali_PerfTestsBlake.html Ali_PerfTestsBlake_$now.html
+  git add Ali_PerfTestsBlake_$now.html
   git commit -m "Checking in jupyter notebook from $now" 
   git push
   rm html_entry 
-  echo "<li><p><strong><a href="blake_nightly_data/Blob_PerfTestsBlake_$now.html">$now</a></strong>.</p>" >& html_entry
+  echo "<li><p><strong><a href="blake_nightly_data/Ali_PerfTestsBlake_$now.html">$now</a></strong>.</p>" >& html_entry
 else  
   echo "$FILE does not exist! Not Jupyter notebook in repo."
 fi
