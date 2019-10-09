e7114048f791513c72b9620044151b6f9c6bd509
Irina K. Tezaur <ikalash@sandia.gov>
2019-10-08 22:06:21 -0700

    Checking in website files for ALI nightly testing of performance.

---
+++ ali/concatenate_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ../blake_ali_nightly_data 
+now=$(date +"%m_%d_%Y")
+FILE=Ali_PerfTestsBlake_$now.html
+echo $FILE
+if test -f "$FILE"; then
+  echo "$FILE file exists"
+  cd ../
+  cat index1 blake_ali_nightly_data/html_entry >& index11
+  mv index11 index1
+  cat index1 index2 >& index.html
+  git add index.html index1 
+  git commit -m "Adding html entry from Ali nightly tests."
+  git push
+else 
+  echo "$FILE does not exist! Not updating index.html."
+fi
---
+++ ali/execute_jupyter_nb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../blake_ali_nightly_data
+git pull 
+jupyter nbconvert --execute Ali_PerfTestsBlake.ipynb
+bash append_date.sh >& append_date.out 
