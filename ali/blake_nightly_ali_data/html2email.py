8b1277f412e952d0e0929c4ee6958e7e7454b8fc
Jerry Watkins <jwatkin@sandia.gov>
2019-10-18 19:25:51 -0600

    Add script to email performance report

---
+++ ali/blake_nightly_ali_data/html2email.py
@@ -0,0 +1,38 @@
+# Import libraries
+from email.mime.text import MIMEText 
+from email.mime.image import MIMEImage 
+from email.mime.multipart import MIMEMultipart
+
+###################################################################################################
+def html2email(subject, html):
+    '''
+    Send email with html string
+    '''
+    # Input
+    sender = 'jwatkin@sandia.gov'
+    #recipients = ['jwatkin@sandia.gov','ikalash@sandia.gov']
+    recipients = ['jwatkin@sandia.gov']
+
+    # Details
+    msgRoot = MIMEMultipart('related')
+    msgRoot['Subject'] = subject
+    msgRoot['From'] = sender
+    msgRoot['To'] = ", ".join(recipients)
+    msgAlternative = MIMEMultipart('alternative')
+    msgRoot.attach(msgAlternative)
+
+    # Alternative text
+    msgText = MIMEText('Alternative plain text.')
+    msgAlternative.attach(msgText)
+
+    # Main message
+    msgText = MIMEText(html,'html')
+    msgAlternative.attach(msgText)
+
+    # Send the email (this example assumes SMTP authentication is required)
+    import smtplib
+    smtp = smtplib.SMTP()
+    smtp.connect('smtp.sandia.gov')
+    smtp.sendmail(sender, recipients, msgRoot.as_string())
+    smtp.quit()
+
