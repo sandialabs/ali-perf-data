805e45b107504fbbf547232fdbf0c1a65086b141
Irina K. Tezaur <ikalash@sandia.gov>
2019-11-02 13:06:44 -0700

    Updating scripts.

--- ali/execute_jupyter_nb.sh
+++ ali/execute_jupyter_nb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd blake_nightly_data
+git reset --hard origin/master
 git pull 
 jupyter nbconvert --execute Ali_PerfTestsBlake.ipynb
 bash append_date.sh >& append_date.out 
--- miniem/execute_jupyter_nb.sh
+++ miniem/execute_jupyter_nb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd blake_nightly_data
+git reset --hard origin/master
 git pull 
 jupyter nbconvert --execute Blob_PerfTestsBlake.ipynb
 bash append_date.sh >& append_date.out 
