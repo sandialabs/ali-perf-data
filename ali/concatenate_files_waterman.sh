be6ab69d4fb7cc2b5b44c00837977f4c43065885
Irina K. Tezaur <ikalash@sandia.gov>
2020-01-25 09:52:02 -0800

    Adding html entry from Ali waterman nightly tests.

--- ali/concatenate_files_waterman.sh
+++ ali/concatenate_files_waterman.sh
@@ -6,11 +6,11 @@ echo $FILE
 if test -f "$FILE"; then
   echo "$FILE file exists"
   cd ../
-  cat index1 waterman_nightly_data/html_entry >& index11
+  cat index1_waterman waterman_nightly_data/html_entry >& index11
   mv index11 index1_waterman
   cat index1_waterman index2 >& index_waterman.html
-  git add index_waterman.html index1_waterman 
-  git commit -m "Adding html entry from Ali nightly tests."
+  git add index_waterman.html index1_waterman
+  git commit -m "Adding html entry from Ali waterman nightly tests."
   git push
 else 
   echo "$FILE does not exist! Not updating index.html."
