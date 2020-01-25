6c9c88f43f222124fdca11e5732dd141a9b52edd
Irina K. Tezaur <ikalash@sandia.gov>
2020-01-25 12:16:05 -0800

    More test script cleanups.

--- ali/process_tests_waterman.sh
+++ ali/process_tests_waterman.sh
@@ -1,6 +1,5 @@
 
 #!/bin/bash
-rm -rf *out
 bash execute_jupyter_nb_waterman.sh >& execute_jupyter_nb_waterman.out
 bash concatenate_files_waterman.sh >& concatenate_files_waterman.sh
 cd waterman_nightly_data
