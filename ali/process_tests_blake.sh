956ce957add1e1b6c492f9c1cd46649e2663a3be
Irina K. Tezaur <ikalash@sandia.gov>
2020-01-26 10:16:50 -0800

    Fixes to scripts.

--- ali/process_tests_blake.sh
+++ ali/process_tests_blake.sh
@@ -1,6 +1,5 @@
 
 #!/bin/bash
-rm -rf *out
 bash execute_jupyter_nb.sh >& execute_jupyter_nb.out
 bash concatenate_files.sh >& concatenate_files.out
 cd blake_nightly_data
--- ali/process_tests_waterman.sh
+++ ali/process_tests_waterman.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 bash execute_jupyter_nb_waterman.sh >& execute_jupyter_nb_waterman.out
-bash concatenate_files_waterman.sh >& concatenate_files_waterman.sh
+bash concatenate_files_waterman.sh >& concatenate_files_waterman.out
 cd waterman_nightly_data
 rm -rf *out
 python email_report.py >& email.out
